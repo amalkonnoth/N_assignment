696ccb292dbae230415dc1fdfd78bd0d267eeab7
amalkonnoth <49062508+amalkonnoth@users.noreply.github.com>
2020-07-26 04:38:40 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask,redirect,url_for,render_template,request,session,flash
+
+app = Flask(__name__)
+app.secret_key = "hello"
+@app.route("/")
+def home():
+	return render_template("home.html")
+
+@app.route("/login",methods=["POST","GET"])
+def login():
+	if request.method == "POST":
+		user = request.form["nm"]
+		session["user"]=user
+		return redirect(url_for("user"))
+	else:
+		if "user" in session:
+			return redirect(url_for("user"))
+		return render_template("login.html")
+
+@app.route("/user")
+def user():
+	if "user" in session:
+		user =session["user"]
+		return render_template("profile.html",username=user)
+	else:
+		flash("please login first","info")
+		return redirect(url_for("login"))
+
+@app.route("/logout")
+def logout():
+	if "user" in session:
+		user =session["user"]
+		flash("You've been logged out!","info")
+		session.pop("user",None)
+	else:
+		flash("please login first","info")
+	
+	return redirect(url_for("login"))
+
+if __name__=="__main__":
+	app.run(debug=True) 
